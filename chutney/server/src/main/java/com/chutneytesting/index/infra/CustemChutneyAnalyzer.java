8c02f4105ddfe0944409b9cb86b3e188c131b81c
nbrouand <7816908+nbrouand@users.noreply.github.com>
2025-02-13 14:32:44 +0100

    Fix analyzer

--- chutney/server/src/main/java/com/chutneytesting/index/infra/CustemChutneyAnalyzer.java
+++ chutney/server/src/main/java/com/chutneytesting/index/infra/CustemChutneyAnalyzer.java
@@ -8,16 +8,20 @@
 package com.chutneytesting.index.infra;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 public class CustemChutneyAnalyzer extends Analyzer {
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
-        Tokenizer source = new StandardTokenizer();
+        Tokenizer source = new WhitespaceTokenizer();
         TokenStream tokenStream = new LowerCaseFilter(source);
         return new TokenStreamComponents(source, tokenStream);
     }
