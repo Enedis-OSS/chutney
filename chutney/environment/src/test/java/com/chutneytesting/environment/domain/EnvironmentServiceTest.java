9456a0e38a0d8e3c668d8b7958ecaa92f4c74de7
DelaunayAlex <138011689+DelaunayAlex@users.noreply.github.com>
2025-04-16 16:26:19 +0200

    feat(environment + ui): Fix deleting env variable + add loader for search in report feature
    ---------
    Co-authored-by: Alex Delaunay <delaunayalexandre18@gmail.com>

--- chutney/environment/src/test/java/com/chutneytesting/environment/domain/EnvironmentServiceTest.java
+++ chutney/environment/src/test/java/com/chutneytesting/environment/domain/EnvironmentServiceTest.java
@@ -11,6 +11,7 @@ import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
@@ -19,11 +20,13 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.chutneytesting.environment.domain.exception.AlreadyExistingEnvironmentException;
+import com.chutneytesting.environment.domain.exception.EnvVariableNotFoundException;
 import com.chutneytesting.environment.domain.exception.InvalidEnvironmentNameException;
 import com.chutneytesting.environment.domain.exception.NoEnvironmentFoundException;
 import com.chutneytesting.environment.domain.exception.SingleEnvironmentException;
 import com.chutneytesting.environment.domain.exception.UnresolvedEnvironmentException;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -78,7 +81,7 @@ class EnvironmentServiceTest {
     }
 
     @Test
-    void should_return_the_unique_env_as_default() {
+    void return_the_unique_env_as_default() {
        // Given
         when(environmentRepository.listNames())
             .thenReturn(List.of("ENV"));
@@ -140,5 +143,74 @@ class EnvironmentServiceTest {
 
         // Then
         verify(environmentRepository, times(1)).delete(eq("ENV"));
+    }
+
+    @Test
+    void delete_variable_from_all_environments_and_update_them() {
+        // Given
+        String key = "API_KEY";
+        List<String> envNames = List.of("dev", "prod");
+
+
+        EnvironmentVariable var1Env1 = new EnvironmentVariable("API_KEY", "123", "ENV1");
+        EnvironmentVariable var2Env1 = new EnvironmentVariable("TOKEN", "abc", "ENV1");
+        Environment env1 = Environment.builder()
+            .withName("ENV1")
+            .withVariables(Set.of(var1Env1, var2Env1))
+            .build();
+
+        EnvironmentVariable var1Env2 = new EnvironmentVariable("API_KEY", "123", "ENV2");
+        EnvironmentVariable var2Env2 = new EnvironmentVariable("TOKEN", "abc", "ENV2");
+        Environment env2 = Environment.builder()
+            .withName("ENV2")
+            .withVariables(Set.of(var1Env2, var2Env2))
+            .build();
+
+        when(environmentRepository.findByNames(envNames)).thenReturn(List.of(env1, env2));
+
+        // When
+        sut.deleteVariable(key, envNames);
+
+        // Then
+        verify(environmentRepository).findByNames(envNames);
+        verify(environmentRepository).save(argThat(env ->
+            env.name.equals("ENV1") &&
+                env.variables.size() == 1 &&
+                env.variables.stream().anyMatch(v -> v.key().equals("TOKEN"))
+        ));
+        verify(environmentRepository).save(argThat(env ->
+            env.name.equals("ENV2") &&
+                env.variables.size() == 1 &&
+                env.variables.stream().anyMatch(v -> v.key().equals("TOKEN"))
+        ));
+    }
+
+    @Test
+    void throw_exception_while_trying_to_delete_not_found_variable_from_all_environments() {
+        // Given
+        String key = "OTHER_KEY";
+        List<String> envNames = List.of("dev", "prod");
+
+
+        EnvironmentVariable var1Env1 = new EnvironmentVariable("API_KEY", "123", "ENV1");
+        EnvironmentVariable var2Env1 = new EnvironmentVariable("TOKEN", "abc", "ENV1");
+        Environment env1 = Environment.builder()
+            .withName("ENV1")
+            .withVariables(Set.of(var1Env1, var2Env1))
+            .build();
+
+        EnvironmentVariable var1Env2 = new EnvironmentVariable("API_KEY", "123", "ENV2");
+        EnvironmentVariable var2Env2 = new EnvironmentVariable("TOKEN", "abc", "ENV2");
+        Environment env2 = Environment.builder()
+            .withName("ENV2")
+            .withVariables(Set.of(var1Env2, var2Env2))
+            .build();
+
+        when(environmentRepository.findByNames(envNames)).thenReturn(List.of(env1, env2));
+
+        // When & Then
+        assertThatThrownBy(() -> sut.deleteVariable(key, envNames))
+            .isInstanceOf(EnvVariableNotFoundException.class)
+            .hasMessage("Variable [" + key + "] not found");
     }
 }
