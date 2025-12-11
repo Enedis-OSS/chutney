964d6422e20158f9e3d39571aa70f363333ee1f1
joelgaspard <4029620+joelgaspard@users.noreply.github.com>
2025-12-11 14:56:55 +0100

    feat: endpoint information on mem-auth and ldap conf

---
+++ chutney/server/src/main/java/fr/enedis/chutney/security/api/AuthenticationConfigDto.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2024 Enedis
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package fr.enedis.chutney.security.api;
+
+public record AuthenticationConfigDto(boolean userPassword) {
+}
