fb860b0018702eba717aa403166f9c0b5f268981
joelgaspard <4029620+joelgaspard@users.noreply.github.com>
2026-04-20 15:22:48 +0200

    feat: access tokens

--- chutney/server/src/main/java/fr/enedis/chutney/security/domain/AuthenticationService.java
+++ chutney/server/src/main/java/fr/enedis/chutney/security/domain/AuthenticationService.java
@@ -7,17 +7,33 @@
 
 package fr.enedis.chutney.security.domain;
 
+import fr.enedis.chutney.security.ApiKeyAuthentication;
+import fr.enedis.chutney.server.core.domain.security.Authorization;
 import fr.enedis.chutney.server.core.domain.security.Role;
 import fr.enedis.chutney.server.core.domain.security.RoleNotFoundException;
 import fr.enedis.chutney.server.core.domain.security.User;
 import fr.enedis.chutney.server.core.domain.security.UserRoles;
+import fr.enedis.chutney.tokens.domain.AccessTokensService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 
 public class AuthenticationService {
 
     private final Authorizations authorizations;
+    private final AccessTokensService accessTokensService;
 
-    public AuthenticationService(Authorizations authorizations) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationService.class);
+
+    private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
+
+    public AuthenticationService(Authorizations authorizations,
+                                 AccessTokensService accessTokensService) {
         this.authorizations = authorizations;
+        this.accessTokensService = accessTokensService;
     }
 
     public Role userRoleById(String userId) {
@@ -25,5 +41,17 @@ public class AuthenticationService {
         User user = userRoles.userById(userId)
             .orElseThrow(() -> RoleNotFoundException.forUser(userId));
         return userRoles.roleByName(user.roleName);
+    }
+
+    public Authentication getAuthentication(HttpServletRequest request) {
+        String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+        LOGGER.info("Trying to authenticate apiKey for request {}", request.getRequestURI());
+
+        if (apiKey == null || !accessTokensService.matchToken(apiKey)) {
+            LOGGER.info("Wrong apiKey for request {}", request.getRequestURI());
+            throw new BadCredentialsException("Invalid API Key");
+        }
+
+        return new ApiKeyAuthentication(AuthorityUtils.createAuthorityList(Authorization.SCENARIO_WRITE.name()));
     }
 }
--- chutney/server/src/test/java/fr/enedis/chutney/security/domain/AuthenticationServiceTest.java
+++ chutney/server/src/test/java/fr/enedis/chutney/security/domain/AuthenticationServiceTest.java
@@ -9,30 +9,38 @@ package fr.enedis.chutney.security.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import fr.enedis.chutney.security.ApiKeyAuthentication;
 import fr.enedis.chutney.server.core.domain.security.Authorization;
 import fr.enedis.chutney.server.core.domain.security.Role;
 import fr.enedis.chutney.server.core.domain.security.RoleNotFoundException;
 import fr.enedis.chutney.server.core.domain.security.User;
 import fr.enedis.chutney.server.core.domain.security.UserRoles;
+import fr.enedis.chutney.tokens.domain.AccessTokensService;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 class AuthenticationServiceTest {
 
     private final Authorizations authorizations = mock(Authorizations.class);
+    private final AccessTokensService accessTokensService = mock(AccessTokensService.class);
     private AuthenticationService sut;
 
     @BeforeEach
     public void setUp() {
-        sut = new AuthenticationService(authorizations);
+        sut = new AuthenticationService(authorizations, accessTokensService);
     }
 
     @Test
-    public void get_role_from_user_id_with_write_implies_read() {
+    void get_role_from_user_id_with_write_implies_read() {
         // Given
         Role expectedRole = Role.builder()
             .withName("expectedRole")
@@ -58,7 +66,7 @@ class AuthenticationServiceTest {
     }
 
     @Test
-    public void throw_user_not_found_when_get_role_for_authentication_for_an_unknown_user() {
+    void throw_user_not_found_when_get_role_for_authentication_for_an_unknown_user() {
         // Given
         when(authorizations.read()).thenReturn(
             UserRoles.builder().build()
@@ -67,5 +75,29 @@ class AuthenticationServiceTest {
         // When
         assertThatThrownBy(() -> sut.userRoleById("unknown-user"))
             .isInstanceOf(RoleNotFoundException.class);
+    }
+
+    @Test
+    void get_api_key_authentication() {
+        HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+        String token = "token";
+        when(httpServletRequest.getHeader(eq("X-API-KEY"))).thenReturn(token);
+        when(accessTokensService.matchToken(token)).thenReturn(true);
+
+        Authentication authentication = sut.getAuthentication(httpServletRequest);
+
+        assertThat(authentication).isInstanceOf(ApiKeyAuthentication.class);
+        assertThat(((ApiKeyAuthentication)authentication).getAuthorities())
+            .containsExactly(new SimpleGrantedAuthority(Authorization.SCENARIO_WRITE.name()));
+    }
+
+    @Test
+    void throw_exception_when_api_key_is_wrong() {
+        HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+        String token = "token";
+        when(httpServletRequest.getHeader(eq("X-API-KEY"))).thenReturn(token);
+        when(accessTokensService.matchToken(token)).thenReturn(false);
+
+        assertThatThrownBy(() -> sut.getAuthentication(httpServletRequest)).isInstanceOf(BadCredentialsException.class);
     }
 }
